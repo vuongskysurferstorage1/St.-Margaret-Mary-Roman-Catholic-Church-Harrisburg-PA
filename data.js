75ebdd91a626aa13aeaaebdb85e621cc9e6a9e38
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-08-25 22:49:36 -0400

    update

--- data.js
+++ data.js
@@ -28,16 +28,52 @@ var APP_DATA = {
       ],
       "faceSize": 3000,
       "initialViewParameters": {
-        "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "pitch": 0,
+        "fov": 1.4134061960355204
       },
       "linkHotspots": [
         {
-          "yaw": 2.919747177298757,
-          "pitch": 0.058827987818570904,
+          "yaw": 0.005223563549790811,
+          "pitch": -0.20532471713245393,
           "rotation": 0,
-          "target": "1-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "4-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": 0.9985395087669637,
+          "pitch": 0.07285055425284526,
+          "rotation": 0,
+          "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": -1.022945924990026,
+          "pitch": 0.11316534534766376,
+          "rotation": 0,
+          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": -0.2714100574022691,
+          "pitch": 1.5014404379397046,
+          "rotation": 0,
+          "target": "7-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": -3.1349921154590312,
+          "pitch": 0.44418038046758745,
+          "rotation": 0,
+          "target": "6-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": 0.7794004430741275,
+          "pitch": 0.007484877312368354,
+          "rotation": 0,
+          "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": 0.005117930955547578,
+          "pitch": 0.34602522655453427,
+          "rotation": 0,
+          "target": "10-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
@@ -76,34 +112,10 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.029679399223727643,
-          "pitch": 0.17423353563380317,
-          "rotation": 0,
-          "target": "0-st-margaret-mary-roman-catholic-church-harrisburg-pa"
-        },
-        {
-          "yaw": 1.3408230535343932,
-          "pitch": 0.13604919600326326,
+          "yaw": 2.919747177298757,
+          "pitch": 0.058827987818570904,
           "rotation": 0,
           "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
-        },
-        {
-          "yaw": 2.307453788999064,
-          "pitch": -0.16623405981186146,
-          "rotation": 0,
-          "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
-        },
-        {
-          "yaw": 1.8845167823855755,
-          "pitch": -0.1488810500071338,
-          "rotation": 0,
-          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
-        },
-        {
-          "yaw": 1.8836848360065659,
-          "pitch": 0.04256915370105929,
-          "rotation": 0,
-          "target": "7-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
@@ -142,34 +154,34 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.665117881932737,
-          "pitch": -0.07043402418597111,
+          "yaw": -0.029679399223727643,
+          "pitch": 0.17423353563380317,
           "rotation": 0,
           "target": "1-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": 0.008882075730024752,
-          "pitch": -0.18440234568395653,
+          "yaw": 1.3408230535343932,
+          "pitch": 0.13604919600326326,
           "rotation": 0,
           "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": -0.6657694518285098,
-          "pitch": -0.14498478019625693,
+          "yaw": 2.307453788999064,
+          "pitch": -0.16623405981186146,
           "rotation": 0,
-          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "4-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": -0.3783302334719423,
-          "pitch": 0.010849782306847189,
+          "yaw": 1.8845167823855755,
+          "pitch": -0.1488810500071338,
           "rotation": 0,
-          "target": "8-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "0-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": -0.7216524533656017,
-          "pitch": 0.013079014561007796,
+          "yaw": 1.8836848360065659,
+          "pitch": 0.04256915370105929,
           "rotation": 0,
-          "target": "6-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "7-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
@@ -208,32 +220,32 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.559216660518672,
-          "pitch": 0.19831071547038803,
+          "yaw": 1.665117881932737,
+          "pitch": -0.07043402418597111,
           "rotation": 0,
           "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": -1.8952460877766182,
-          "pitch": 0.13062835093995062,
-          "rotation": 0,
-          "target": "1-st-margaret-mary-roman-catholic-church-harrisburg-pa"
-        },
-        {
-          "yaw": 1.3255622879863935,
-          "pitch": 0.2686092803267659,
+          "yaw": 0.008882075730024752,
+          "pitch": -0.18440234568395653,
           "rotation": 0,
           "target": "4-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": 0.0002632290937221171,
-          "pitch": 0.026842533355825537,
+          "yaw": -0.6657694518285098,
+          "pitch": -0.14498478019625693,
           "rotation": 0,
-          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "0-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": -0.002456194779952625,
-          "pitch": 0.23766901683202768,
+          "yaw": -0.3783302334719423,
+          "pitch": 0.010849782306847189,
+          "rotation": 0,
+          "target": "8-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": -0.7216524533656017,
+          "pitch": 0.013079014561007796,
           "rotation": 0,
           "target": "6-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         }
@@ -274,40 +286,34 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.19083942922372188,
-          "pitch": -0.19795747051984236,
+          "yaw": -1.559216660518672,
+          "pitch": 0.19831071547038803,
           "rotation": 0,
           "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": 0.7405469263339981,
-          "pitch": -0.15655345884129446,
+          "yaw": -1.8952460877766182,
+          "pitch": 0.13062835093995062,
+          "rotation": 0,
+          "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": 1.3255622879863935,
+          "pitch": 0.2686092803267659,
           "rotation": 0,
           "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": 0.26041848076049945,
-          "pitch": 0.015018818763170572,
+          "yaw": 0.0002632290937221171,
+          "pitch": 0.026842533355825537,
           "rotation": 0,
-          "target": "8-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "0-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": 0.7417909797852964,
-          "pitch": 0.01913817848050492,
-          "rotation": 0,
-          "target": "7-st-margaret-mary-roman-catholic-church-harrisburg-pa"
-        },
-        {
-          "yaw": 1.0433029679204733,
-          "pitch": 0.022588117190014856,
+          "yaw": -0.002456194779952625,
+          "pitch": 0.23766901683202768,
           "rotation": 0,
           "target": "6-st-margaret-mary-roman-catholic-church-harrisburg-pa"
-        },
-        {
-          "yaw": 0.32352647302211324,
-          "pitch": -0.1812982680319486,
-          "rotation": 0,
-          "target": "9-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
@@ -346,46 +352,40 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.005223563549790811,
-          "pitch": -0.20532471713245393,
-          "rotation": 0,
-          "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
-        },
-        {
-          "yaw": 0.9985395087669637,
-          "pitch": 0.07285055425284526,
-          "rotation": 0,
-          "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
-        },
-        {
-          "yaw": -1.022945924990026,
-          "pitch": 0.11316534534766376,
+          "yaw": -0.19083942922372188,
+          "pitch": -0.19795747051984236,
           "rotation": 0,
           "target": "4-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": -0.2714100574022691,
-          "pitch": 1.5014404379397046,
+          "yaw": 0.7405469263339981,
+          "pitch": -0.15655345884129446,
+          "rotation": 0,
+          "target": "0-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": 0.26041848076049945,
+          "pitch": 0.015018818763170572,
+          "rotation": 0,
+          "target": "8-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": 0.7417909797852964,
+          "pitch": 0.01913817848050492,
           "rotation": 0,
           "target": "7-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": -3.1349921154590312,
-          "pitch": 0.44418038046758745,
+          "yaw": 1.0433029679204733,
+          "pitch": 0.022588117190014856,
           "rotation": 0,
           "target": "6-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
-          "yaw": 0.7794004430741275,
-          "pitch": 0.007484877312368354,
+          "yaw": 0.32352647302211324,
+          "pitch": -0.1812982680319486,
           "rotation": 0,
-          "target": "1-st-margaret-mary-roman-catholic-church-harrisburg-pa"
-        },
-        {
-          "yaw": 0.005117930955547578,
-          "pitch": 0.34602522655453427,
-          "rotation": 0,
-          "target": "10-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "9-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
@@ -433,25 +433,25 @@ var APP_DATA = {
           "yaw": -0.003094512035543673,
           "pitch": -0.26205402158559643,
           "rotation": 0,
-          "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "4-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": 0.020008618025151748,
           "pitch": -0.9231237137568336,
           "rotation": 0,
-          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "0-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": 0.7590157066796763,
           "pitch": -0.0315192854287325,
           "rotation": 0,
-          "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": -0.8333403049099424,
           "pitch": -0.03784413434736322,
           "rotation": 0,
-          "target": "4-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
@@ -493,7 +493,7 @@ var APP_DATA = {
           "yaw": -0.3347306856718628,
           "pitch": -1.4881012325745342,
           "rotation": 0,
-          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "0-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": -3.138009874534564,
@@ -505,7 +505,7 @@ var APP_DATA = {
           "yaw": -0.0013430617099690068,
           "pitch": -0.31458628824216106,
           "rotation": 0,
-          "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "4-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": 0.0004460875929588326,
@@ -517,19 +517,19 @@ var APP_DATA = {
           "yaw": -1.0690372823580745,
           "pitch": -0.010864509062184169,
           "rotation": 0,
-          "target": "4-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": 0.9852678764049401,
           "pitch": -0.05289433566362156,
           "rotation": 0,
-          "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": 0.6963446714708716,
           "pitch": -0.07860758184311578,
           "rotation": 0,
-          "target": "1-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
@@ -571,13 +571,13 @@ var APP_DATA = {
           "yaw": -1.396706447150379,
           "pitch": 0.013418743428452728,
           "rotation": 0,
-          "target": "4-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": 1.2891304489861284,
           "pitch": 0.020137812653398868,
           "rotation": 0,
-          "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": 0.0022205743761709584,
@@ -601,7 +601,7 @@ var APP_DATA = {
           "yaw": -3.111965912361109,
           "pitch": -0.23673440664366652,
           "rotation": 0,
-          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "0-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": -3.1112564915736325,
@@ -655,7 +655,7 @@ var APP_DATA = {
           "yaw": 0.004375706323941131,
           "pitch": -0.30104730739827623,
           "rotation": 0,
-          "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "4-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": -3.1352688387454357,
@@ -667,7 +667,7 @@ var APP_DATA = {
           "yaw": -1.2494918456537736,
           "pitch": 0.09420753403763626,
           "rotation": 0,
-          "target": "4-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": 1.5342253040068785,
@@ -679,13 +679,13 @@ var APP_DATA = {
           "yaw": 1.2127341326000458,
           "pitch": 0.059145229484439454,
           "rotation": 0,
-          "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": 0.9508593767802545,
           "pitch": -0.006020115433752693,
           "rotation": 0,
-          "target": "1-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
@@ -745,7 +745,7 @@ var APP_DATA = {
           "yaw": -3.1180729267693366,
           "pitch": -0.35039300896411163,
           "rotation": 0,
-          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "0-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": -3.1107969001571227,
@@ -757,19 +757,19 @@ var APP_DATA = {
           "yaw": 1.2262704351395843,
           "pitch": 0.012481552663752282,
           "rotation": 0,
-          "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "3-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": -1.2736489480684448,
           "pitch": 0.026166208046335626,
           "rotation": 0,
-          "target": "4-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "5-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         },
         {
           "yaw": 0.968493847183872,
           "pitch": -0.028516076587060013,
           "rotation": 0,
-          "target": "1-st-margaret-mary-roman-catholic-church-harrisburg-pa"
+          "target": "2-st-margaret-mary-roman-catholic-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
